a3f34a59f67c8739d6785f7daef273a2b8af7a94
Claire Manley <cmanley2@zagmail.gonzaga.edu>
2020-01-21 10:31:59 -0800

    Done w/ App.

--- FounderButtons2/ViewController.swift
+++ FounderButtons2/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel1: UILabel!
+    
+  
+    @IBOutlet weak var messageLabel2: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        messageLabel1.text = "  "
+        messageLabel2.text = "  "
     }
 
+    @IBAction func googlePressed(_ sender: UIButton) {
+        messageLabel1.text = "Larry Page"
+        messageLabel2.text = "Sergey Brin"
+        messageLabel2.textColor = UIColor.blue
+    }
+    
+    @IBAction func runwayPressed(_ sender: UIButton) {
+        messageLabel1.text = "Jennifer Hyman"
+        messageLabel2.text = "Jenny Fleiss"
+        messageLabel2.textColor = UIColor.red
 
+    }
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+        messageLabel1.text = " "
+        messageLabel2.text = " "
+    }
+    
 }
 
